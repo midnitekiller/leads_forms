de45a50804ece96fcac8867b6ffff30da48efb43
midnitekiller <liefjillcolegado@gmail.com>
2019-02-26 11:53:39 +0800

    asdf

---
+++ assets/main.js
@@ -0,0 +1,13 @@
+/*
+|================================|
+|===selector button Add Rooms===|
+|================================|
+*/
+ $(document).ready(function(){
+    $('#addleads').on('click',function(){
+        window.location.href='leads/leads.html';
+    });
+    $('#btnaddleads').on('click',function(){
+        window.location.href='leads/leads.html';
+    });
+ });
--- model/Database.php
+++ model/Database.php
@@ -1,5 +1,4 @@
 <?php 
-
 class Database {
     function dbConn(){
         $dbhost = "localhost";
